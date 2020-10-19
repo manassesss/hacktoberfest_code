5b8635151e1ec7fab85f4aedbde02aee38ee2e78
Pedro Lucca <48389824+PedroLucca@users.noreply.github.com>
2020-10-19 15:49:09 -0300

    second commit

---
+++ im_rich.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I am Rich!"),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png")
+          ),
+        ),
+      ),
+    ),
+  );
+}
